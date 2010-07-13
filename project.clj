f43a24fa1dade67c0995788cf9c79437a029bc4c
Brenton Ashworth <bashwort@gmail.com>
2010-07-13 10:02:34 -0700

    Put uncompiled version in Clojars.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject difform "1.0.1"
+(defproject org.clojars.brenton/difform "1.0.1"
   :description "Diff clojure forms!"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [google-diff-match-patch "0.1"]])
+                 [google-diff-match-patch "0.1"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])

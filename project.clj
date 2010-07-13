b7b514df84af44e46779efe27602a17d3ca153fb
Brenton Ashworth <bashwort@gmail.com>
2010-07-13 11:27:40 -0700

    Depend on version of goole-diff-match-patch that is compiled with Java 1.5.

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "Diff clojure forms!"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [google-diff-match-patch "0.1"]]
+                 [org.clojars.brenton/google-diff-match-patch "0.1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])

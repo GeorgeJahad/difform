40a1dd6792af2a57c8c9a77f7c6fbbf8ee74462c
Brenton Ashworth <bashwort@gmail.com>
2010-08-18 21:36:04 -0700

    Update for use the Clojure 1.2.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.brenton/difform "1.0.2"
+(defproject org.clojars.brenton/difform "1.1.0"
   :description "Diff clojure forms!"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
+                 [org.clojure/clojure-contrib "1.2.0-RC3"]
                  [org.clojars.brenton/google-diff-match-patch "0.1"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])

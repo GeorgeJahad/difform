f0095053203c596d2c59fb7d6b4d582c2b5f25e0
Brenton Ashworth <bashwort@gmail.com>
2010-10-28 14:40:01 -0700

    Use Clojure 1.2 as a dev dependency.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.brenton/difform "1.1.0"
+(defproject org.clojars.brenton/difform "1.1.1"
   :description "Diff clojure forms!"
-  :dependencies [[org.clojure/clojure "1.2.0-RC3"]
-                 [org.clojure/clojure-contrib "1.2.0-RC3"]
-                 [org.clojars.brenton/google-diff-match-patch "0.1"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
+  :dependencies [[org.clojars.brenton/google-diff-match-patch "0.1"]]
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
